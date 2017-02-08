b0c3b2937654414896c9ddd1a2134344cfc9bb16
naigo <lorenzo.niccolai94@gmail.com>
2017-02-08 19:08:34 +0100

    Commenti su parsing in parser.py

---
+++ parser.py
@@ -0,0 +1,23 @@
+'''
+
+dictionary = create_dictionary(documents)
+all_labels = read_all_labels()
+all_documents = read_all_documents()
+
+foreach document : all_documents
+    document.instances = create_instances(document.text)
+    foreach instance : document.instances
+        instance.mega_vector = create_mega_vector(dictionary, instance.words)
+    foreach label : all_labels
+        if label in document.labels
+            document.labels_vector.push(+1)
+        else
+            document.labels_vector.push(-1)
+
+Questa procedura in teoria si può eseguire una volta sola in assoluto: se l'output si potesse salvare su un file non sarebbe male.
+
+foreach document : all_documents
+    file.write(document.mega_vector)
+    file.write(document.labels_vector)
+
+'''
