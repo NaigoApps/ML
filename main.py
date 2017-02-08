a0fe94e341c5b3bef4d1c702883898bddbe4f4e6
Fabio <rospovitto92@gmail.com>
2017-02-08 01:29:31 +0100

    Separata la classe parser

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-import xml.etree.ElementTree as ET
-tree = ET.parse('country_data.xml')
-root = tree.getroot()
+import parserFile
+
+
+if __name__ == "__main__":
+    # Open the first Reuters data set and create the parser
+    filename = "dataset/reut2-000.sgm"
+    parser = parserFile.ReutersParser()
+
+    # Parse the document and force all generated docs into
+    # a list so that it can be printed out to the console
+    doc = parser.parse(open(filename, 'rb'))
+    print list(doc)[0][0]
+    #pprint.pprint(list(doc))
