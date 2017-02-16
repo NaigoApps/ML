9db6c5ecac5b306ccc47bdbd20238100f2b23091
naigo <lorenzo.niccolai94@gmail.com>
2017-02-16 10:45:20 +0100

    Merge branch 'master' of https://github.com/NaigoApps/ML
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -1,13 +1,55 @@
 import parserFile
-
+from miml_svm import MiMlSVM
+import random
 
 if __name__ == "__main__":
     # Open the first Reuters data set and create the parser
-    filename = "dataset/reut2-000.sgm"
-    parser = parserFile.ReutersParser()
+    # filename = "dataset/reut2-000.sgm"
+    # parser = parserFile.ReutersParser()
 
     # Parse the document and force all generated docs into
     # a list so that it can be printed out to the console
-    doc = parser.parse(open(filename, 'rb'))
-    print list(doc)[0][0]
-    #pprint.pprint(list(doc))
+    # doc = parser.parse(open(filename, 'rb'))
+    # print list(doc)[0][0]
+    # pprint.pprint(list(doc))
+    datasetSize = 100
+    testsetSize = 20
+    featuresNumber = 2
+    labelsNumber = 3
+    minInstances = 5
+    maxInstances = 20
+    dataset = []
+    for document in range(datasetSize):
+        instances = []
+        for instance in range(random.randint(minInstances, maxInstances + 1)):
+            features = []
+            for feature in range(0, featuresNumber):
+                features.append(random.random())
+            instances.append(features)
+        dataset.append(instances)
+
+    labels = []
+    for document in range(datasetSize):
+        docLabels = []
+        for label in range(labelsNumber):
+            if(random.random() < 0.5):
+                docLabels.append(-1)
+            else:
+                docLabels.append(+1)
+        labels.append(docLabels)
+
+
+    miml = MiMlSVM()
+    miml.train(dataset, labels)
+
+    testset = []
+    for document in range(datasetSize):
+        instances = []
+        for instance in range(random.randint(minInstances, maxInstances + 1)):
+            features = []
+            for feature in range(0, featuresNumber):
+                features.append(random.random())
+            instances.append(features)
+        testset.append(instances)
+
+    print miml.test(testset)
---
+++ miml_svm.py
@@ -0,0 +1,25 @@
+import misvm
+import numpy
+
+class MiMlSVM:
+    def __init__(self):
+        self.SVMs = None;
+
+    def train(self, dataset, labels):
+        self.SVMs = []
+        index = 0
+        for label in numpy.transpose(labels):
+            classifier = misvm.MISVM(kernel='linear', C=1.0, max_iters=50)
+            classifier.fit(dataset, label)
+            self.SVMs.append(classifier)
+            index += 1
+            print "Trained " + str(index) + " of " + str(len(label))
+
+    def test(self, test_set):
+        all_labels = []
+        index = 0
+        for SVM in self.SVMs:
+            all_labels.append(SVM.predict(test_set))
+            index += 1
+            print "Tested " + str(index) + " labels of " + str(len(self.SVMs))
+        return numpy.transpose(all_labels)
