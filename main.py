abb4f3cdf0099e1dce1054e250beff5483238c2c
naigo <lorenzo.niccolai94@gmail.com>
2017-03-18 12:05:45 +0100

    Merge branch 'master' of https://github.com/NaigoApps/ML
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -1,55 +1,151 @@
 import parserFile
 from miml_svm import MiMlSVM
 import random
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import numpy as np
+
+
+class Pixel:
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+
+
+class Rect:
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+
+class BigRect:
+    def __init__(self, x, y, w, h, parts):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.rects = []
+        for r in range(parts):
+            for c in range(parts):
+                self.rects.append(Rect(x + float(c) / parts * w,
+                                       y + float(r) / parts * h,
+                                       w / parts,
+                                       h / parts))
+
+
+def generateRect(rows, cols, minS, maxS, parts):
+    x = int(random.random() * cols)
+    y = int(random.random() * rows)
+    w = int(min(cols - x, random.random() * (maxS - minS) + minS))
+    h = int(min(rows - y, random.random() * (maxS - minS) + minS))
+    return BigRect(x, y, w, h, parts)
+
+
+def randomRect(rows, cols):
+    x = int(random.random() * cols)
+    y = int(random.random() * rows)
+    w = int(random.random() * (cols - x))
+    h = int(random.random() * (rows - y))
+    return Rect(x, y, w, h)
+
+
+def stretch(l, r, x):
+    x = (x - l) / (r - l)
+    x = max(x, l)
+    x = min(x, r)
+    return x
+
+
+def calcLabel(rect, img):
+    colors = [-1, -1, -1]
+    for r in range(int(rect.y), int(rect.y + rect.h)):
+        for c in range(int(rect.x), int(rect.x + rect.w)):
+            if img[r][c][0] == 1:
+                colors[0] = 1
+            if img[r][c][1] == 1:
+                colors[1] = 1
+            if img[r][c][2] == 1:
+                colors[2] = 1
+    return colors
+
+def convert(img, rects):
+    dataset = []
+    labels = []
+    for bigRect in rects:
+        document = []
+        for littleRect in bigRect.rects:
+            document.append([littleRect.x, littleRect.y, littleRect.w, littleRect.h])
+        dataset.append(document)
+        labels.append(calcLabel(bigRect, img))
+    return (dataset, labels)
+
 
 if __name__ == "__main__":
-    # Open the first Reuters data set and create the parser
-    # filename = "dataset/reut2-000.sgm"
-    # parser = parserFile.ReutersParser()
 
-    # Parse the document and force all generated docs into
-    # a list so that it can be printed out to the console
-    # doc = parser.parse(open(filename, 'rb'))
-    # print list(doc)[0][0]
-    # pprint.pprint(list(doc))
-    datasetSize = 100
-    testsetSize = 20
-    featuresNumber = 2
-    labelsNumber = 3
-    minInstances = 5
-    maxInstances = 20
-    dataset = []
-    for document in range(datasetSize):
-        instances = []
-        for instance in range(random.randint(minInstances, maxInstances + 1)):
-            features = []
-            for feature in range(0, featuresNumber):
-                features.append(random.random())
-            instances.append(features)
-        dataset.append(instances)
+    nRows = 10
+    nCols = 10
+    steps = 1
 
-    labels = []
-    for document in range(datasetSize):
-        docLabels = []
-        for label in range(labelsNumber):
-            if(random.random() < 0.5):
-                docLabels.append(-1)
-            else:
-                docLabels.append(+1)
-        labels.append(docLabels)
+    minRectSize = 5
+    maxRectSize = 20
 
+    datasetSize = 200
+    trainingSetSize = 199
+    testSetSize = 1
 
-    miml = MiMlSVM()
-    miml.train(dataset, labels)
+    image = np.zeros((nRows, nCols, 3))
+    for s in range(steps):
+        rect = randomRect(nRows, nCols)
+        image[rect.y: rect.y + rect.h, rect.x: rect.x + rect.w] = [1, 0, 0]
+        rect = randomRect(nRows, nCols)
+        image[rect.y: rect.y + rect.h, rect.x: rect.x + rect.w] = [0, 1, 0]
+        rect = randomRect(nRows, nCols)
+        image[rect.y: rect.y + rect.h, rect.x: rect.x + rect.w] = [0, 0, 1]
 
-    testset = []
-    for document in range(datasetSize):
-        instances = []
-        for instance in range(random.randint(minInstances, maxInstances + 1)):
-            features = []
-            for feature in range(0, featuresNumber):
-                features.append(random.random())
-            instances.append(features)
-        testset.append(instances)
+    _ , p = plt.subplots(1, 1)
+    p.imshow(image)
 
-    print miml.test(testset)
+    rawDataset = []
+
+    for i in range(datasetSize):
+        rawDataset.append(generateRect(nRows, nCols, minRectSize, maxRectSize, int(random.random() * 2 + 2)))
+
+    (dataset, labels) = convert(image, rawDataset)
+
+    for i, r in enumerate(rawDataset):
+        # for sr in r.rects:
+            # p[1].add_patch(Rectangle((sr.x, sr.y), sr.w, sr.h, fill=False, edgecolor="red"))
+        p.add_patch(Rectangle((r.x, r.y), r.w, r.h, fill=False, edgecolor=[(l + 1) / 2 * 0.7 for l in labels[i]]))
+
+    plt.xlim((0, nCols))
+    plt.ylim((0, nRows))
+    plt.show()
+
+    trainingSet = dataset[0: trainingSetSize]
+    trainingSetLabels = labels[0: trainingSetSize]
+    testSet = dataset[trainingSetSize::]
+    testSetLabels = labels[trainingSetSize::]
+
+    svm = MiMlSVM()
+    svm.train(trainingSet, trainingSetLabels)
+    result = svm.test(testSet)
+
+    comparison = np.sign(testSetLabels) - np.sign(result)
+
+    ok = 0
+    perfect = 0
+    for entry in comparison:
+        if (entry == [0,0,0]).all():
+            perfect += 1
+        if entry[0] == 0:
+            ok += 1
+        if entry[1] == 0:
+            ok += 1
+        if entry[2] == 0:
+            ok += 1
+
+    print "Labels ok: " + str(ok) + " of " + str(len(comparison)*3) + " : " + str(ok*100/len(comparison)/3) + "%"
+    print "Labels perfect: " + str(perfect) + " of " + str(len(comparison)) + " : " + str(perfect*100/len(comparison)) + "%"
--- miml_svm.py
+++ miml_svm.py
@@ -9,11 +9,12 @@ class MiMlSVM:
         self.SVMs = []
         index = 0
         for label in numpy.transpose(labels):
-            classifier = misvm.MISVM(kernel='linear', C=1.0, max_iters=50)
+            # classifier = misvm.MISVM(kernel='linear', C=1.0, max_iters=50)
+            classifier = misvm.SIL(kernel='linear', C=1.0)
             classifier.fit(dataset, label)
             self.SVMs.append(classifier)
             index += 1
-            print "Trained " + str(index) + " of " + str(len(label))
+            print "Trained " + str(index) + " of " + str(len(numpy.transpose(labels)))
 
     def test(self, test_set):
         all_labels = []
